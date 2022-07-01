86beb7745e0f84209330850b51339f249d620562
slytherin96 <60347794+slytherin96@users.noreply.github.com>
2022-07-01 11:57:06 +0500

    Правки ДЗ 4

--- java part 2/lesson4/src/main/java/СalculationFormula.java
+++
@@ -1,8 +0,0 @@
-public class СalculationFormula {
-    public float[] calculation(float[] array) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-        return array;
-    }
-}
