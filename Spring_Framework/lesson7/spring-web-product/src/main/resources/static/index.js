7898ce053a120e2f7dd8cc8db4b2d1736a037299
slytherin96 <60347794+slytherin96@users.noreply.github.com>
2022-12-22 20:34:56 +0500

    ДЗ 7

---
+++ Spring_Framework/lesson7/spring-web-product/src/main/resources/static/index.js
@@ -0,0 +1,45 @@
+angular.module('app', []).controller('indexController', function ($scope, $http) {
+    const contextPath = 'http://localhost:8191/app';
+
+    $scope.loadProducts = function () {
+        $http.get(contextPath + '/products')
+            .then(function (response) {
+                $scope.ProductsList = response.data;
+            });
+    };
+
+    $scope.deleteProduct = function (productId) {
+        $http.get(contextPath + '/products/delete/' + productId)
+            .then(function (response) {
+                $scope.loadProducts();
+            });
+    }
+
+    $scope.changeScore = function (productId, delta) {
+        $http({
+            url: contextPath + '/products/change_score',
+            method: 'POST',
+            params: {
+                productId: productId,
+                delta: delta
+            }
+        }).then(function (response) {
+            $scope.loadProducts();
+        });
+    }
+
+    $scope.addProduct = function (title, cost) {
+        $http({
+            url: contextPath + '/products/add',
+            method: 'POST',
+            params: {
+                title: title,
+                cost: cost
+            }
+        }).then(function (response) {
+            $scope.loadProducts();
+        });
+    }
+
+    $scope.loadProducts();
+});
